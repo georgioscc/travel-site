1b4ea4e983e7eff6452ba5360957b816325a9720
Georgios Chatzigeorgakidis <hello@georgios.cc>
2024-01-29 17:49:15 +0200

    Triggering class toggle for mobile menu on click.

---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,19 @@
+class MobileMenu {
+	constructor() {
+		this.menuIcon = document.querySelector(".site-header__menu-icon")
+		this.menuContent = document.querySelector(".site-header__menu-content")
+		this.events()
+	}
+
+	events() {
+		this.menuIcon.addEventListener("click", () => this.toggleTheMenu()) // The arrow function here does not manipulate the value of the "this" keyword.'
+		// If we did not provide an arrow function here, and instead listed a direct refererence to the toggleTheMenu method, then addEventListener would have run this code within a context where "this" (in toggleTheMenu) would be pointing towards the element that got clicked.
+		// However, because we provided an arrow function so that the "this" keyword doesn't change, "this" in toggleTheMenu is still pointing towards our overall object, which lets us easily access our menuContent property.
+	}
+
+	toggleTheMenu() {
+		this.menuContent.classList.toggle("site-header__menu-content--is-visible")
+	}
+}
+
+export default MobileMenu
